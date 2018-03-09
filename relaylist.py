f2e9af0b0275fdc69d6d38a49f1863b8273526be
Matt Traudt <sirmatt@ksu.edu>
2018-03-08 20:46:02 -0500

    Add relay list

---
+++ relaylist.py
@@ -0,0 +1,55 @@
+import util.stem as stem_utils
+from stem import Flag
+import time
+
+
+class RelayList:
+    REFRESH_INTERVAL = 300  # seconds
+
+    def __init__(self):
+        # self.refresh_event = PeriodicEvent(
+        #     self.refresh, _run_interval=self.REFRESH_INTERVAL,
+        #     _run_at_end=False)
+        self._controller = stem_utils.init_controller()
+        self._refresh()
+
+    @property
+    def relays(self):
+        if time.time() >= self._last_refresh + self.REFRESH_INTERVAL:
+            self._refresh()
+        return self._relays
+
+    @property
+    def exits(self):
+        return self._relays_with_flag(Flag.EXIT)
+
+    @property
+    def guards(self):
+        return self._relays_with_flag(Flag.GUARD)
+
+    @property
+    def hsdirs(self):
+        return self._relays_with_flag(Flag.HSDIR)
+
+    @property
+    def unmeasured(self):
+        relays = self.relays
+        return [r for r in relays if r.is_unmeasured]
+
+    @property
+    def measured(self):
+        relays = self.relays
+        return [r for r in relays if not r.is_unmeasured]
+
+    def _relays_with_flag(self, flag):
+        relays = self.relays
+        return [r for r in relays if flag in r.flags]
+
+    def _init_relays(self):
+        c = self._controller
+        assert stem_utils.is_controller_okay(c)
+        return [ns for ns in c.get_network_statuses()]
+
+    def _refresh(self):
+        self._relays = self._init_relays()
+        self._last_refresh = time.time()
---
+++ util/stem.py
@@ -0,0 +1,53 @@
+from stem.control import Controller
+from stem import SocketError
+
+__all__ = ['init_controller', 'is_controller_okay']
+
+DEFAULT_ATTEMPTS = [
+    ('socket', '/var/run/tor/control'),
+    ('port', 9051),
+    ('port', 9151),
+]
+
+
+def init_controller():
+    for cont_type, cont_location in DEFAULT_ATTEMPTS:
+        if cont_type == 'socket':
+            c = _init_controller_socket(cont_location)
+            if not c:
+                continue
+        elif cont_type == 'port':
+            c = _init_controller_port(cont_location)
+            if not c:
+                continue
+        else:
+            raise RuntimeError('Unknown controller type {}'.format(cont_type))
+        return c
+
+
+def is_controller_okay(c):
+    if not c:
+        return False
+    return c.is_alive() and c.is_authenticated()
+
+
+def _init_controller_port(port):
+    assert isinstance(port, int)
+    try:
+        c = Controller.from_port(port=port)
+    except SocketError:
+        return None
+    # TODO: Allow for auth via more than just CookieAuthentication
+    c.authenticate()
+    return c
+
+
+def _init_controller_socket(socket):
+    assert isinstance(socket, str)
+    try:
+        c = Controller.from_socket_file(path=socket)
+    except SocketError:
+        return None
+    # TODO: Allow for auth via more than just CookieAuthentication
+    c.authenticate()
+    return c

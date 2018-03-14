53b627c6b6ac0fbaf9f52ab3ec1e766a89898d63
Matt Traudt <sirmatt@ksu.edu>
2018-03-14 13:06:40 +0100

    Add toy "echo" server

---
+++ server.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+import sys
+import socket
+from threading import Thread
+
+send_amount = None
+
+def new_thread(sock):
+    def closure():
+        sock.send(b'a' * send_amount)
+        try:
+            sock.shutdown(socket.SHUT_RDWR)
+            sock.close()
+        except:
+            pass
+    thread = Thread(target=closure)
+    return thread
+
+def main():
+    global send_amount
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    h = (sys.argv[1], int(sys.argv[2]))
+    send_amount = int(sys.argv[3])
+    print('listening on', h)
+    server.bind(h)
+    server.listen(5)
+    try:
+        while True:
+            sock, addr = server.accept()
+            print('accepting connection from', addr)
+            t = new_thread(sock)
+            t.run()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        server.close()
+
+
+def usage():
+    print(sys.argv[0], 'bind-ip bind-port send-amount')
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 4:
+        usage()
+        exit(1)
+    main()
+

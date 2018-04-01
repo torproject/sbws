8f0b4f924ddc651596306ea91df78dc868d593db
Matt Traudt <sirmatt@ksu.edu>
2018-04-01 14:27:07 -0400

    Buffer any log lines generated during config parsing.

--- sbws/__main__.py
+++ sbws/__main__.py
@@ -36,11 +36,37 @@ def create_parser():
     return p
 
 
+def _log_buffer():
+    ''' Little helper for when you want to log some things before the
+    PastlyLogger is created. use log_buf['add'] like you would print(), and
+    use log_buf['dump'] to get all lines that were buffered as an iterable.
+
+    >>> log_buf = _log_buffer()
+    >>> log_buf['add']('Hello, world')
+    >>> log_buf['add']('This is the second line')
+    >>> for line in log_buf['dump']():
+    >>>     print(line)
+
+    '''
+    buf = []
+
+    def add(*s):
+        buf.append(' '.join([str(_) for _ in s]))
+
+    def dump():
+        while len(buf):
+            yield buf.pop(0)
+    return {'add': add, 'dump': dump}
+
+
 def main():
+    log_buf = _log_buffer()
     parser = create_parser()
     args = parser.parse_args()
-    conf = get_config(args)
+    conf = get_config(args, log_fn=log_buf['add'])
     log = make_logger(args, conf)
+    for line in log_buf['dump']():
+        log.debug(line)
     conf_valid, conf_errors = validate_config(conf)
     if not conf_valid:
         for e in conf_errors:

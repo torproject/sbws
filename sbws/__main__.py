ff309dcd8764d6e51b23e14f6e345412e67dcb95
Matt Traudt <sirmatt@ksu.edu>
2018-04-04 21:37:50 -0400

    Move argparse creator to its own file

--- sbws/__main__.py
+++ sbws/__main__.py
@@ -6,36 +6,10 @@ import sbws.commands.server
 import sbws.commands.stats
 from sbws.util.config import get_config
 from sbws.util.config import validate_config
+from sbws.util.parser import create_parser
 from sbws.globals import make_logger
-from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
-import os
 
 VERSION = '0.1.0'
-
-
-def _default_dot_sbws_dname():
-    home = os.path.expanduser('~')
-    return os.path.join(home, '.sbws')
-
-
-def create_parser():
-    p = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
-    p.add_argument(
-        '-v', '--verbose', action='count', default=0,
-        help='Increase log level verbosity from the configured value')
-    p.add_argument(
-        '-q', '--quiet', action='count', default=0,
-        help='Decrease log level verbosity from the configured value')
-    p.add_argument('-d', '--directory', default=_default_dot_sbws_dname(),
-                   help='Name of the .sbws directory')
-    sub = p.add_subparsers(dest='command')
-    sbws.commands.client.gen_parser(sub)
-    sbws.commands.generate.gen_parser(sub)
-    sbws.commands.init.gen_parser(sub)
-    sbws.commands.pwgen.gen_parser(sub)
-    sbws.commands.server.gen_parser(sub)
-    sbws.commands.stats.gen_parser(sub)
-    return p
 
 
 def _log_buffer():
---
+++ sbws/util/parser.py
@@ -0,0 +1,33 @@
+import sbws.commands.client
+import sbws.commands.generate
+import sbws.commands.init
+import sbws.commands.pwgen
+import sbws.commands.server
+import sbws.commands.stats
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+import os
+
+
+def _default_dot_sbws_dname():
+    home = os.path.expanduser('~')
+    return os.path.join(home, '.sbws')
+
+
+def create_parser():
+    p = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
+    p.add_argument(
+        '-v', '--verbose', action='count', default=0,
+        help='Increase log level verbosity from the configured value')
+    p.add_argument(
+        '-q', '--quiet', action='count', default=0,
+        help='Decrease log level verbosity from the configured value')
+    p.add_argument('-d', '--directory', default=_default_dot_sbws_dname(),
+                   help='Name of the .sbws directory')
+    sub = p.add_subparsers(dest='command')
+    sbws.commands.client.gen_parser(sub)
+    sbws.commands.generate.gen_parser(sub)
+    sbws.commands.init.gen_parser(sub)
+    sbws.commands.pwgen.gen_parser(sub)
+    sbws.commands.server.gen_parser(sub)
+    sbws.commands.stats.gen_parser(sub)
+    return p

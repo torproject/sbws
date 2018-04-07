ceb0da4fcab4f49bdfd7fe698eed5d0f34ac7165
Matt Traudt <sirmatt@ksu.edu>
2018-04-06 22:44:26 -0400

    Move MockPastlyLogger to common file

--- tests/commands/test_stats.py
+++ tests/commands/test_stats.py
@@ -8,26 +8,7 @@ import sbws.commands.stats
 from datetime import date
 import os
 import time
-
-
-class MockPastlyLogger:
-    def __init__(self, *a, **kw):
-        pass
-
-    def debug(self, *s):
-        print(*s)
-
-    def info(self, *s):
-        return self.debug(*s)
-
-    def notice(self, *s):
-        return self.info(*s)
-
-    def warn(self, *s):
-        return self.notice(*s)
-
-    def error(self, *s):
-        return self.warn(*s)
+from tests.common import MockPastlyLogger
 
 
 def init_directory(dname):

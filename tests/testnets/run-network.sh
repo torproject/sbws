c7e498822022f34c36be922d44435b3fc379a1c0
Matt Traudt <sirmatt@ksu.edu>
2018-04-10 21:27:22 -0400

    Add script to run an integration test, but doesn't check results yet

---
+++ tests/testnets/run-network.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+set -e
+function cleanup {
+	kill $(jobs -p)
+	echo -n ''
+}
+trap cleanup EXIT
+
+function usage {
+	echo "Usage: $0 <net>"
+	echo "Where <net> is one of: $(find . -mindepth 1 -maxdepth 1 -type d)"
+}
+
+[ "$1" == "" ] && usage && exit 1 || net="$1"
+[ ! -d "$net" ] && usage && exit 1
+
+pushd $net
+./01-gen-configs.sh
+./02-start-network.sh
+sleep 5
+echo 'Waiting until network is ready ...'
+time ./03-network-in-ready-state.py auth* relay* exit*
+echo 'All ready!'
+
+sbws -d . server > debug.server.log &
+sleep 1
+sbws -d . client > debug.client.log &
+
+run_time="45"
+echo "Running for $run_time seconds ..."
+sleep $run_time
+
+sbws -d . generate | tee generate.log
+sbws -d . stats | tee stats.log
+
+./04-stop-network.sh

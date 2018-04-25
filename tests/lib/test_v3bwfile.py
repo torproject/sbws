ee5ffba81bbb2548aa0243e251724e6c86e57ea4
juga0 <juga@riseup.net>
2018-04-25 14:28:48 +0000

    Fix E302 linter error

--- tests/lib/test_v3bwfile.py
+++ tests/lib/test_v3bwfile.py
@@ -10,6 +10,7 @@ def test_v3bwheader_str():
     assert str(header) == '{}\nversion=1.1.0\nsoftware=sbws\n' \
                           'software_version=0.1.0\n'.format(timestamp)
 
+
 def test_v3bwfile():
     """Test generate v3bw file (including relay_lines)."""
     pass

3c54f14aed16cf37a067149ddff3df28d9617120
juga0 <juga@riseup.net>
2018-04-27 20:36:49 +0000

    Change header str order

--- tests/lib/test_v3bwfile.py
+++ tests/lib/test_v3bwfile.py
@@ -12,6 +12,26 @@ def test_v3bwheader_str():
                           'software_version={}\n'.format(timestamp, version)
 
 
+def test_v3bwheader_earlier_result_str():
+    """Test header str with earlier_result"""
+    timestamp = 1524661857
+    earlier_result_ts = 1523887747.2904038
+    header = V3BwHeader(timestamp, earlier_result_ts=earlier_result_ts)
+    assert str(header) == '{}\nearlier_result=1523887747.2904038' \
+                          '{}\nversion=1.1.0\nsoftware=sbws\n' \
+                          'software_version={}\n'.format(timestamp, version)
+
+
+def test_v3bwheader_scanner_started_str():
+    """Test header str with scanner_started"""
+    timestamp = 1524661857
+    scanner_started_ts = 1523887747.2904038
+    header = V3BwHeader(timestamp, scanner_started_ts=scanner_started_ts)
+    assert str(header) == '{}\nscanner_started=1523887747.2904038' \
+                          '{}\nversion=1.1.0\nsoftware=sbws\n' \
+                          'software_version={}\n'.format(timestamp, version)
+
+
 def test_v3bwfile():
     """Test generate v3bw file (including relay_lines)."""
     pass

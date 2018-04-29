8ee187b5da511eb0c09afa5364e1a8821b22d032
Matt Traudt <sirmatt@ksu.edu>
2018-04-29 08:34:34 -0400

    Fix test with hardcoded sbws version

--- tests/lib/test_v3bwfile.py
+++ tests/lib/test_v3bwfile.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """Test generation of bandwidth measurements document (v3bw)"""
 from sbws.lib.v3bwfile import V3BwHeader
+from sbws import __version__ as version
 
 
 def test_v3bwheader_str():
@@ -8,7 +9,7 @@ def test_v3bwheader_str():
     timestamp = 1524661857
     header = V3BwHeader(timestamp)
     assert str(header) == '{}\nversion=1.1.0 software=sbws ' \
-                          'software_version=0.1.0\n'.format(timestamp)
+                          'software_version={}\n'.format(timestamp, version)
 
 
 def test_v3bwfile():

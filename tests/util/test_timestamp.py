9da99a0778a549d456bd47c9bb6a1ba0b4b97fa7
juga0 <juga@riseup.net>
2018-05-25 05:10:01 +0000

    Add test for timestamp conversion functions

---
+++ tests/util/test_timestamp.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""Test timestamp conversion util functions"""
+from datetime import datetime
+
+from sbws.util.timestamp import (dt_obj_to_isodt_str, unixts_to_dt_obj,
+                                 unixts_to_isodt_str, unixts_to_str)
+
+
+isodt_str = '2018-05-23T12:55:04'
+dt_obj = datetime.strptime(isodt_str, '%Y-%m-%dT%H:%M:%S')
+unixts = int(dt_obj.timestamp())
+
+
+def test_dt_obj_to_isodt_str():
+    assert isodt_str == dt_obj_to_isodt_str(dt_obj)
+
+
+def test_unixts_to_dt_obj():
+    assert dt_obj == unixts_to_dt_obj(unixts)
+
+
+def test_unixts_to_isodt_str():
+    assert isodt_str == unixts_to_isodt_str(unixts)
+
+
+def test_unixts_to_str():
+    assert str(unixts) == unixts_to_str(unixts)

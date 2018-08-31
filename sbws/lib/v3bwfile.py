2b98827f23a6163c162db6dc1c1287053583060d
juga0 <juga@riseup.net>
2018-08-31 11:19:31 +0000

    fixup! Move cap to avg bw inside of scale method

--- sbws/lib/v3bwfile.py
+++ sbws/lib/v3bwfile.py
@@ -252,8 +252,9 @@ class V3BWLine(object):
         kwargs.update(cls.result_types_from_results(results))
         # useful args for scaling
         if success_results:
-            # maybe it should be the last
-            kwargs['desc_avg_bw_bs'] = results[0].relay_average_bandwidth
+            # the most recent should be the last
+            kwargs['desc_avg_bw_bs'] = \
+                success_results[-1].relay_average_bandwidth
             kwargs['rtt'] = cls.rtt_from_results(success_results)
             bw = cls.bw_bs_median_from_results(success_results)
             kwargs['bw_bs_mean'] = cls.bw_bs_mean_from_results(success_results)

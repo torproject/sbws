19da64c866d2dabdd474dbe717c3c3ab11a9931d
juga0 <juga@riseup.net>
2018-09-05 12:26:46 +0000

    Use args to filter bw lines
    when creating a bw file from results

--- sbws/lib/v3bwfile.py
+++ sbws/lib/v3bwfile.py
@@ -469,8 +469,9 @@ class V3BWFile(object):
         log.info('Processing results to generate a bandwidth list file.')
         header = V3BWHeader.from_results(results, state_fpath)
         bw_lines_raw = []
-        for fp in results.keys():
-            line = V3BWLine.from_results(results[fp])
+        for fp, values in results.items():
+            line = V3BWLine.from_results(values, secs_recent, secs_away,
+                                         min_num)
             if line is not None:
                 bw_lines_raw.append(line)
         if not bw_lines_raw:

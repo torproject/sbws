6597f3554445b57d78eebfb6beea20f60332a6fd
juga0 <juga@riseup.net>
2018-09-25 08:00:26 +0000

    Fix formula scaping latex in docstring

--- sbws/lib/v3bwfile.py
+++ sbws/lib/v3bwfile.py
@@ -695,45 +695,45 @@ class V3BWFile(object):
         All of that can be expressed as:
 
         .. math::
-           :label:torflow_bwn_eq
-
-            bwn_i =&
-                max\left(
-                    \frac{bw_i}{\mu},
-                    \frac{bwf_i}{\mu_{bwf}}
-                    \right)
-                \times bwobs_i\\
+        
+            bwn_i &=
+                max\\left(
+                    \\frac{bw_i}{\\mu},
+                    \\frac{bwf_i}{\\mu_{bwf}}
+                    \\right)
+                \\times bwobs_i
 
         .. math::
 
-             bwn_i =&
-                max\left(
-                    \frac{bw_i}{\mu},
-                    min \left(
+             bwn_i &=
+                max\\left(
+                    \\frac{bw_i}{\\mu},
+                    min \\left(
                         bw_i,
-                        bw_i \times \mu
-                        \right)
-                            \times
-                            \frac{bw}{\sum_{i=1}^{n}
-                            min \left(bw_i,
-                                bw_i \times \mu
-                        \right)}
-                    \right)
-                \times bwobs_i \\
-              =&
-                max\left(
-                  \frac{bw_i}{\frac{\sum_{i=1}^{n}bw_i}{n}},
-                  min \left(
-                      bw_i,
-                      bw_i \times \frac{\sum_{i=1}^{n}bw_i}{n}
-                      \right)
-                          \times
-                          \frac{bw}{\sum_{i=1}^{n}
-                          min \left(bw_i,
-                              bw_i \times \frac{\sum_{i=1}^{n}bw_i}{n}
-                      \right)}
-                  \right)
-              \times bwobs_i
+                        bw_i \\times \\mu
+                        \\right)
+                            \\times
+                            \\frac{bw_i}{\\sum_{i=1}^{n}
+                            min \\left(bw_i,
+                                bw_i \\times \\mu
+                            \\right)}
+                    \\right)
+                \\times bwobs_i \\
+        
+             &=
+                max\\left(
+                    \\frac{bw_i}{\\frac{\\sum_{i=1}^{n}bw_i}{n}},
+                    min \\left(
+                        bw_i,
+                        bw_i \\times \\frac{\\sum_{i=1}^{n}bw_i}{n}
+                        \\right)
+                            \\times
+                            \\frac{bw_i}{\\sum_{i=1}^{n}
+                            min \\left(bw_i,
+                                bw_i \\times \\frac{\\sum_{i=1}^{n}bw_i}{n}
+                            \\right)}
+                    \\right)
+                \\times bwobs_i
 
         """
         log.info("Calculating relays' bandwidth using Torflow method.")

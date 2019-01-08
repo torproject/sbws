6ba839c7e46a47de8699e581777a46ff606a5ef6
juga0 <juga@riseup.net>
2019-01-08 14:46:52 +0000

    settings: add class to store end event singleton

--- sbws/__init__.py
+++ sbws/__init__.py
@@ -1,1 +1,20 @@
 __version__ = '1.0.3-dev0'
+
+
+import threading
+
+from . import globals  # noqa
+
+
+class Settings:
+    def __init__(self):
+        # update this dict from globals (but only for ALL_CAPS settings)
+        for setting in dir(globals):
+            if setting.isupper():
+                setattr(self, setting, getattr(globals, setting))
+        self.end_event = threading.Event()
+
+    def set_end_event(self):
+        self.end_event.set()
+
+settings = Settings()  # noqa

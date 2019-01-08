ea50a75040cbed3db363147788d6088347e8969e
juga0 <juga@riseup.net>
2019-01-08 14:46:52 +0000

    settings: add class to store end event singleton

--- sbws/__init__.py
+++ sbws/__init__.py
@@ -1,1 +1,25 @@
 __version__ = '1.0.3-dev0'
+
+
+import threading  # noqa
+
+
+class Settings:
+    """Singleton settings for all the packages.
+    This way change settings can be seen by all the packages that import it.
+
+    It lives in ``__init__.py`` to leave open the possibility of having a
+    ``settings.py`` module for user settings.
+
+    .. note:: After refactoring, globals should only have constants.
+      Any other variable that needs to be modified when initializing
+      should be initialized here.
+
+    """
+    def __init__(self):
+        self.end_event = threading.Event()
+
+    def set_end_event(self):
+        self.end_event.set()
+
+settings = Settings()  # noqa

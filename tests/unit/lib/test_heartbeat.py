969bfbbfc32af87b424dbbff168f2f4992033326
George Kadianakis <desnacked@riseup.net>
2019-03-21 19:48:44 +0200

    Bake more details into the heartbeat module and out of the main loop.

--- tests/unit/lib/test_heartbeat.py
+++ tests/unit/lib/test_heartbeat.py
@@ -1,21 +1,23 @@
 """Unit tests for heartbeat"""
 import logging
-import time
 
 from sbws.lib import heartbeat
 
 
 def test_total_measured_percent(conf, caplog):
-    measured_percent = 0
-    measured_fp_set = set(['A', 'B'])
-    main_loop_tstart = time.monotonic()
-    relays_fingerprints = set(['A', 'B', 'C'])
+    hbeat = heartbeat.Heartbeat(conf.getpath('paths', 'state_fname'))
+
+    hbeat.register_consensus_fprs(['A', 'B', 'C'])
+
+    hbeat.register_measured_fpr('A')
+    hbeat.register_measured_fpr('B')
 
     caplog.set_level(logging.INFO)
-    new_measured_percent = heartbeat.total_measured_percent(
-            measured_percent, relays_fingerprints, measured_fp_set,
-            main_loop_tstart, conf.getpath('paths', 'state_fname')
-            )
-    assert new_measured_percent == 67
+
+    assert hbeat.previous_measurement_percent == 0
+
+    hbeat.print_heartbeat_message()
+
+    assert hbeat.previous_measurement_percent == 67
     caplog.records[1].getMessage().find("Measured in total 2 (67%)")
     caplog.records[2].getMessage().find("1 relays still not measured")

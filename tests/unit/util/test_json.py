757ecaf3265c1d47ede54fe9e3fa9a4613523729
juga0 <juga@riseup.net>
2020-03-14 16:52:21 +0000

    chg: json: Create custom JSON encoder/decoder
    to be able to serialize/deserailize datetime in the state file.

---
+++ tests/unit/util/test_json.py
@@ -0,0 +1,56 @@
+"""json.py unit tests."""
+import json
+
+from sbws.util.json import CustomDecoder, CustomEncoder
+
+STATE = """{
+    "min_perc_reached": null,
+    "recent_consensus_count": [
+        "2020-03-04T10:00:00",
+        "2020-03-05T10:00:00",
+        "2020-03-06T10:00:00"
+    ],
+    "recent_measurement_attempt": [
+        [
+            "2020-03-04T10:00:00",
+            2
+        ],
+        [
+            "2020-03-05T10:00:00",
+            2
+        ],
+        [
+            "2020-03-06T10:00:00",
+            2
+        ]
+    ],
+    "recent_priority_list": [
+        "2020-03-04T10:00:00",
+        "2020-03-05T10:00:00",
+        "2020-03-06T10:00:00"
+    ],
+    "recent_priority_relay": [
+        [
+            "2020-03-04T10:00:00",
+            2
+        ],
+        [
+            "2020-03-05T10:00:00",
+            2
+        ],
+        [
+            "2020-03-06T10:00:00",
+            2
+        ]
+    ],
+    "scanner_started": "2020-03-14T16:15:22",
+    "uuid": "x"
+}"""
+
+
+def test_decode_encode_roundtrip():
+    d = json.loads(STATE, cls=CustomDecoder)
+    s = json.dumps(d, cls=CustomEncoder, indent=4, sort_keys=True)
+    assert s == STATE
+    d1 = json.loads(s, cls=CustomDecoder)
+    assert d == d1

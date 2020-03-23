aeec130443d995fba1c7cf2fee4af099924bacfa
juga0 <juga@riseup.net>
2020-03-23 09:21:50 +0000

    fix: test: Check that log prints a number
    and not a list of timestamps.

--- tests/unit/lib/test_heartbeat.py
+++ tests/unit/lib/test_heartbeat.py
@@ -2,9 +2,12 @@
 import logging
 
 from sbws.lib import heartbeat
+from sbws.util.state import State
 
 
 def test_total_measured_percent(conf, caplog):
+    state = State(conf["paths"]["state_fname"])
+    state["recent_priority_list"] = [1, 2, 3]
     hbeat = heartbeat.Heartbeat(conf.getpath('paths', 'state_fname'))
 
     hbeat.register_consensus_fprs(['A', 'B', 'C'])
@@ -19,6 +22,7 @@ def test_total_measured_percent(conf, caplog):
     hbeat.print_heartbeat_message()
 
     assert hbeat.previous_measurement_percent == 67
+    assert 0 == caplog.records[0].getMessage().find("Run 3 main loops.")
     assert 0 == caplog.records[1].getMessage().find(
         "Measured in total 2 (67%)"
     )

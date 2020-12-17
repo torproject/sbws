212047f4c4601d263078cde4338060704d026d78
juga0 <juga@riseup.net>
2020-12-17 15:30:11 +0000

    fix: test: Add chutney configuration
    and scripts to run the integration tests with chutney.
    It does not replace yet the way integration tests are run.

---
+++ tests/integration/start_chutney.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -x
+
+CURRENT_DIR=`pwd`
+CHUTNEY_DIR=${1:-./chutney}
+
+# If chutney dir already exists, this will fail but it doesn't matter.
+git clone https://git.torproject.org/chutney.git $CHUTNEY_DIR
+
+cp tests/integration/chutney_data/bwscanner $CHUTNEY_DIR/networks
+cp tests/integration/chutney_data/*.tmpl $CHUTNEY_DIR/torrc_templates
+
+cd $CHUTNEY_DIR
+# In case it wasn't cloned recently, pull.
+# Since this is run only for the tests, it's ok if the tests fail with a newer
+# chutney version, so that we can detect it early.
+git pull
+
+# Stop chutney network if it is already running
+./chutney stop networks/bwscanner
+./chutney configure networks/bwscanner
+./chutney start networks/bwscanner
+./chutney status networks/bwscanner
+./chutney wait_for_bootstrap networks/bwscanner
+
+cd $CURRENT_DIR
